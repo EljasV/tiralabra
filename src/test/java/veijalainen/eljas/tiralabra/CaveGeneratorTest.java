3c2a5cb5fa9f93cdd31cac562f8fdbd1068065df
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2023-03-09 16:42:00 +0200

    Testi

--- src/test/java/veijalainen/eljas/tiralabra/CaveGeneratorTest.java
+++ src/test/java/veijalainen/eljas/tiralabra/CaveGeneratorTest.java
@@ -1,34 +1,98 @@
 package veijalainen.eljas.tiralabra;
 
+import org.junit.Test;
 import veijalainen.eljas.tiralabra.util.Pair;
 
-import java.awt.image.BufferedImage;
 import java.util.*;
 
-public class CaveGenerator {
-	static Random random = new Random();
+import static org.junit.Assert.assertTrue;
+
+public class CaveGeneratorTest {
 
 
-	/**
-	 * Luo luoaston. Eriytetty tänne Mainista helppokäyttöisyyden vuoksi.
-	 *
-	 * @param width  Luolaston korkeus
-	 * @param height Luolaston leveys
-	 * @param nRooms Huoneiden minimiäärä
-	 * @return Kuva luolastosta
-	 */
-	static BufferedImage process(int width, int height, int nRooms) {
-		//
-		//Ohjelma luo tarvittavat tietorakenteet ja kuvan
-		//
-
+	@Test
+	public void roomsAreConnected() {
+		int width = 1000;
+		int height = 1000;
+		int nRooms = 200;
 		SpacePartitioning spacePartitioning = new SpacePartitioning(width, height, nRooms);
 		DoublyConnectedEdgeList doublyConnectedEdgeList = new DoublyConnectedEdgeList();
 		Grid grid = new Grid(width, height);
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		int perlinScale = 150;
-		PerlinNoise perlinNoise = new PerlinNoise((int) Math.ceil((double) width / perlinScale), (int) Math.ceil((double) height / perlinScale));
 
+		Set<RoomInfo> roomInfos = new HashSet<>();
+
+		createSolidGrid(spacePartitioning, doublyConnectedEdgeList, grid, roomInfos);
+
+		class pos {
+			final int x;
+			final int y;
+
+			pos(int x, int y) {
+				this.x = x;
+				this.y = y;
+			}
+
+			@Override
+			public boolean equals(Object o) {
+				if (this == o) return true;
+				if (o == null || getClass() != o.getClass()) return false;
+
+				pos pos = (pos) o;
+
+				if (x != pos.x) return false;
+				return y == pos.y;
+			}
+
+			@Override
+			public int hashCode() {
+				int result = x;
+				result = 31 * result + y;
+				return result;
+			}
+		}
+
+		RoomInfo first = roomInfos.stream().findAny().get();
+
+		Set<pos> visited = new HashSet<>();
+		Queue<pos> queue = new ArrayDeque<>();
+
+		pos firstPos = new pos(first.midX, first.midY);
+
+		queue.add(firstPos);
+		visited.add(firstPos);
+
+		while (!queue.isEmpty()) {
+			pos inspectedPos = queue.poll();
+
+			pos[] neighbours = {
+					  new pos(inspectedPos.x + 1, inspectedPos.y),
+					  new pos(inspectedPos.x - 1, inspectedPos.y),
+					  new pos(inspectedPos.x, inspectedPos.y + 1),
+					  new pos(inspectedPos.x, inspectedPos.y - 1)
+			};
+
+			for (pos neighbourPos :
+					  neighbours) {
+				if (visited.contains(neighbourPos)) {
+					continue;
+				}
+				if (grid.getSafe(neighbourPos.x, neighbourPos.y, TileType.GrassFloor) != TileType.DefinetlyFloor) {
+					continue;
+				}
+				queue.add(neighbourPos);
+				visited.add(neighbourPos);
+			}
+
+		}
+
+		for (RoomInfo roomInfo :
+				  roomInfos) {
+			pos roomCenter = new pos(roomInfo.midX, roomInfo.midY);
+			assertTrue(visited.contains(roomCenter));
+		}
+	}
+
+	private static void createSolidGrid(SpacePartitioning spacePartitioning, DoublyConnectedEdgeList doublyConnectedEdgeList, Grid grid, Set<RoomInfo> roomInfos) {
 		//
 		// Käydään SpacePartitioning läpi ja lisätään alueet DoublyConnectedEdgeListaan, luodaan jokaiselle alueelle oma huone ja piirretään huoneet.
 		//
@@ -37,6 +101,7 @@ public class CaveGenerator {
 			DoublyConnectedEdgeList.Face face = doublyConnectedEdgeList.addRectangle(x, y, node.width, node.height);
 			face.roomInfo = roomInfo;
 
+			roomInfos.add(roomInfo);
 
 			grid.fillArea(roomInfo.innerX, roomInfo.innerY, roomInfo.innerWidth, roomInfo.innerHeight, TileType.DefinetlyFloor);
 
@@ -78,8 +143,8 @@ public class CaveGenerator {
 		//
 		// Piirtää Gridiin käytävät
 		//
-		corridorGenerator.connections.forEach((roomInfo1, roomInfos) -> {
-			roomInfos.forEach(roomInfo2Pair -> {
+		corridorGenerator.connections.forEach((roomInfo1, roomInfosPairs) -> {
+			roomInfosPairs.forEach(roomInfo2Pair -> {
 				int edgeMidX = (roomInfo2Pair.right.origin.x + roomInfo2Pair.right.next.origin.x) / 2;
 				int edgeMidY = (roomInfo2Pair.right.origin.y + roomInfo2Pair.right.next.origin.y) / 2;
 				grid.fillLine(roomInfo1.midX, roomInfo1.midY, edgeMidX, edgeMidY, TileType.DefinetlyFloor);
@@ -90,54 +155,7 @@ public class CaveGenerator {
 		//
 		// Iteroi soluautomaattia
 		//
-		/*for (int i = 0; i < 1000; i++) {
-			grid.iterate();
-		}*/
-
 		grid.iterateUntilStable();
-
-		//
-		// Muunna Gridin arvot yhtenäiseksi ja koristele kartta
-		//
 		grid.solidify();
-		grid.decorate(perlinNoise, perlinScale);
-
-		//
-		// Tietorakenteiden avulla piirretään kuva
-		// Huomio! Tällä hetkellä kuvaan pirretään huoneet ja käytävät valkoisella ja vaaleanharmaalla. Seinät piiretään mustalla ja tumman harmaalla
-
-
-		grid.visit((x, y, tileType) -> {
-			switch (tileType) {
-				case MaybeWall:
-					image.setRGB(x, y, 0x66_66_66);
-					break;
-				case MaybeFloor:
-					image.setRGB(x, y, 0xAA_AA_AA);
-					break;
-				case DefinetlyWall:
-					image.setRGB(x, y, 0x00_00_00);
-					break;
-				case DefinetlyFloor:
-					image.setRGB(x, y, 0xFF_FF_FF);
-					break;
-				case StoneWall:
-					image.setRGB(x, y, 0x30_30_30);
-					break;
-				case StoneFloor:
-					image.setRGB(x, y, 0xAA_AA_AA);
-					break;
-				case GrassWall:
-					image.setRGB(x, y, 0x00_4E_14);
-					break;
-				case GrassFloor:
-					image.setRGB(x, y, 0x3A_D3_4A);
-					break;
-				default:
-					image.setRGB(x, y, 0xFF_00_FF);
-			}
-		});
-
-		return image;
 	}
 }
